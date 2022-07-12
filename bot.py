1e49e1eee9d8e683c7e0bae08c07e2e6c3269bf9
ss9214 <109015229+ss9214@users.noreply.github.com>
2022-07-12 17:22:36 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,17 +6,62 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import wavelink
-
+import asyncpg
+import asyncio
+import aiohttp
+import logging
 load_dotenv()
 
 # token and guild
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-
+DEFAULT_PREFIX = '!'
 # intents
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+##################### prefix stuff #################################
+cache = {995741045851689010: "!"}
+
+
+# get current prefix
+async def get_prefix(bot, message):
+    for key, value in cache.items():
+        try:
+            prefix = cache[key]
+        except KeyError:  # not cached
+            prefix = await bot.db.fetchval('SELECT prefix FROM guilds WHERE guild_id = $1', message.guild.id)
+            if prefix is None:
+                prefix = DEFAULT_PREFIX
+                cache[message.guild.id] = prefix
+        else:  # cached
+            if prefix is None:
+                prefix = DEFAULT_PREFIX
+        return commands.when_mentioned_or(prefix)(bot, message)
+
+# set prefix
+bot = commands.Bot(command_prefix=get_prefix, intents=intents)
+
+
+# change prefix
+@bot.command(aliases=['setpre'], help="Change the prefix of the server.")
+@commands.has_permissions(administrator=True)
+async def setprefix(ctx, new_prefix):
+    await bot.db.execute('UPDATE guilds SET prefix = $1 WHERE guild_id = $2', new_prefix, ctx.guild.id)
+    cache[ctx.guild.id] = new_prefix
+    await ctx.send("Prefix Updated")
+
+
+# check prefix
+@bot.command(aliases=['pre'], help="Check what the current prefix of this server is.")
+@commands.guild_only()
+async def prefix(ctx):
+    for key in cache.keys():
+        try:
+            ctx.guild.id = key
+        except KeyError:  # not cached
+            await ctx.send("The prefix of this server is !")
+        else:  # cached
+            await ctx.send(f"The prefix of this server is {cache[ctx.guild.id]}")
 
 
 # Log in text
@@ -34,15 +79,6 @@ async def on_member_join(member):
         await guild.system_channel.send(
             f'Hi {member.mention}! Welcome to {guild.name}!'
         )
-
-
-# what is the prefix
-@bot.listen('on_message')
-async def on_message(message):
-    if message.author == bot.user:
-        return
-    if message.content == "prefix":
-        await message.channel.send("!")
 
 
 ############################# random commands #############################
@@ -280,7 +316,7 @@ async def nowp(ctx):
     else:
         vc: wavelink.Player = ctx.voice_client
     if vc.is_playing() or vc.is_paused():
-        duration = str(int(vc.source.duration/60)) + ":"
+        duration = str(int(vc.source.duration / 60)) + ":"
         if int(vc.source.duration % 60) == 0:
             duration += "00"
         elif 0 < int(vc.source.duration % 60) < 10:
@@ -310,8 +346,21 @@ async def nextp(ctx):
             duration += str(int(next_song.duration % 60))
         else:
             duration += str(int(next_song.duration % 60))
-        await ctx.send(f"The bot is next playing {next_song.title} **{duration}** in channel {ctx.me.voice.channel.mention}")
+        await ctx.send(
+            f"The bot is next playing {next_song.title} **{duration}** in channel {ctx.me.voice.channel.mention}")
     elif vc.queue.is_empty is True:
         return await ctx.send("There is nothing in queue.")
 
-bot.run(TOKEN)
+
+################################# Create Database #############################################
+logging.basicConfig(level=logging.INFO)
+
+
+async def main():
+    async with aiohttp.ClientSession() as ahttp, \
+            asyncpg.create_pool(dsn='postgres://postgres:postgres@localhost:5432/TreehouseBot') as db_pool:
+        bot.ahttp = ahttp
+        bot.db = db_pool
+        await bot.start(TOKEN)
+
+asyncio.run(main())
