c806137e663b03b3c442caee2c05135925b7cf22
ss9214 <109015229+ss9214@users.noreply.github.com>
2022-07-15 00:34:51 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions, MissingPermissions
 
+
 class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
