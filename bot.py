0a0c236efc019b5fac739e6272be8a4bedde34b0
ss9214 <109015229+ss9214@users.noreply.github.com>
2022-07-22 22:27:16 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,31 +8,32 @@ import asyncpg
 import asyncio
 import aiohttp
 import logging
-
+from discord import Permissions
 
 load_dotenv()
 
 # token and guild
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-DEFAULT_PREFIX = '!'
+DEFAULT_PREFIX = 'T'
 # intents
 intents = discord.Intents.all()
-cache = {"00000000000000": "!"}
-activity = discord.Game(name="ur mom")
+cache = {"00000000000000": "a"}
+activity = discord.Game(name="ur mom | Thelp")
 
 
 # metaclass
 class MyCogs(commands.Bot):
     async def setup_hook(self):
-        initial_extensions = ['cogs.music',
-                              'cogs.fun',
-                              'cogs.economy',
-                              'cogs.admin',
-                              'cogs.leveling',
+        initial_extensions = ['cogs.admin',
+                              'cogs.moderation',
                               'cogs.utility',
-                              'cogs.antispam',
-                              'cogs.colors']
+                              'cogs.music',
+                              'cogs.economy',
+                              'cogs.fun',
+                              'cogs.colors',
+                              'cogs.leveling',
+                              'cogs.antispam']
         for extension in initial_extensions:
             await self.load_extension(extension)
 
@@ -55,8 +56,61 @@ async def get_prefix(bot, message):
 
 
 bot = MyCogs(get_prefix, intents=intents, activity=activity, status=discord.Status.idle)
+
+
+############################## HELP ###########################################################
+class MyHelpCommand(commands.HelpCommand):
+
+    def __init__(self):
+        super().__init__()
+
+    async def send_bot_help(self, mapping):
+        em = discord.Embed(title="Help",
+                           description=f"**Do '<@995715935287648348> prefix' if you forgot the prefix of your server.**",
+                           color=discord.Color.purple())
+        channel = self.get_destination()
+        try:
+            prefix = cache[channel.guild.id]
+        except KeyError:  # not cached
+            prefix = await bot.db.fetchval('SELECT prefix FROM guilds WHERE guild_id = $1', channel.guild.id)
+            if not prefix:
+                prefix = "T"
+        for cog, commands in mapping.items():
+            command_signatures = [command.name for command in commands]
+            for i in range(len(command_signatures)):
+                string_a = bot.get_command(command_signatures[i]).signature
+                string_b = string_a.replace("=None", "")
+                string_c = string_b.replace("[", "<")
+                string_d = string_c.replace("]", ">")
+                command = command_signatures[i]
+                command_signatures[i] = "**" + prefix + command_signatures[i] + "**"
+                command_signatures[i] += f' {string_d} - {bot.get_command(command).description}'
+            if command_signatures:
+                cog_name = getattr(cog, "qualified_name", "No Category")
+                emoji = ""
+                if cog_name == "Music":
+                    emoji = '<:musicnotes:996810296956026910>'
+                elif cog_name == "Fun":
+                    emoji = ':tada:'
+                elif cog_name == "Economy":
+                    emoji = '<:money:996812142021976186>'
+                elif cog_name == "Admin":
+                    emoji = ':gear:'
+                elif cog_name == "Levels":
+                    emoji = ':star_struck:'
+                elif cog_name == "Utility":
+                    emoji = ':straight_ruler:'
+                elif cog_name == "Colors":
+                    emoji = ':art:'
+                elif cog_name == "Moderation":
+                    emoji = '<:ban:998413036425596978>'
+                if cog_name != "No Category":
+                    em.add_field(name=f'__{cog_name}__  {emoji}', value=f"\n".join(command_signatures), inline=False)
+        await channel.send(embed=em)
+
+
+bot.help_command = MyHelpCommand()
 bot.cache = cache
-spam_dict = {0000000000: ["word", "word", "word", "word"]}
 
 
 # Log in text
@@ -69,41 +123,33 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     guild = member.guild
+    guild_roles = guild.roles
+    guild_channels = guild.channels
+    verify_channel = await bot.db.fetchval('SELECT verify_channel FROM verify WHERE guild_id = $1',
+                                           guild.id)
+    if not verify_channel:  # not in database
+        pass
+    else:
+        unverified_role = None
+        templist = ["Unverified", "unverified"]
+        for role in guild_roles:
+            if role.name in templist:
+                unverified_role = role
+                break
+        if not unverified_role:
+            unverified_role = await guild.create_role(name="Unverified", permissions=Permissions(view_channel=True))
+            overwrites = {unverified_role: discord.PermissionOverwrite(view_channel=False)}
+            templist = ["verification", "Verification", "rules", "Rules"]
+            for channel in guild_channels:
+                if not channel.permissions_synced:
+                    if channel.name not in templist:
+                        await channel.edit(overwrites=overwrites)
+        await member.add_roles(unverified_role)
     if guild.system_channel is not None:
         await guild.system_channel.send(
             f'Hi {member.mention}! Welcome to {guild.name}!'
         )
 
-
-############################## HELP ###########################################################
-class MyHelp(commands.HelpCommand):
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help")
-        for cog, commands in mapping.items():
-            command_signatures = [self.get_command_signature(c) for c in commands]
-            if command_signatures:
-                cog_name = getattr(cog, "qualified_name", "No Category")
-                emoji = ""
-                if cog_name == "Music":
-                    emoji = '<:musicnotes:996810296956026910>'
-                elif cog_name == "Fun":
-                    emoji = ':tada:'
-                elif cog_name == "Economy":
-                    emoji = '<:money:996812142021976186>'
-                elif cog_name == "Admin":
-                    emoji = ':gear:'
-                elif cog_name == "Leveling":
-                    emoji = ':star_struck:'
-                elif cog_name == "Utility":
-                    emoji = ':straight_ruler:'
-                elif cog_name == "Colors":
-                    emoji = ':art:'
-                embed.add_field(name=f'__{cog_name}__  {emoji}', value="\n".join(command_signatures), inline=True)
-        channel = self.get_destination()
-        await channel.send(embed=embed)
-
-
-bot.help_command = MyHelp()
 
 ################################# Create Database #############################################
 logging.basicConfig(level=logging.INFO)
@@ -118,3 +164,4 @@ async def main():
 
 
 asyncio.run(main())
+bg_task = loop.create_task(self.check_for_birthday())
